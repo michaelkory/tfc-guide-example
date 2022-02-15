e6211ac8dd7e485f1ca67eb74e61dc4c659da412
Michael Kory Wilson <michael@wilson.org>
2022-02-14 21:23:01 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,12 +18,22 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
-resource "aws_instance" "ubuntu" {
+resource "aws_instance" "ubuntu1" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = var.instance_type
 
   tags = {
     Name                 = var.instance_name
+    "Linux Distribution" = "Ubuntu"
+  }
+}
+
+resource "aws_instance" "ubuntu2" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = var.instance_type
+
+  tags = {
+    Name                 = "instance 2"
     "Linux Distribution" = "Ubuntu"
   }
 }
